8f7c826f4b33afda53cc03a200aceba3e00831c8
nataliefrancis <natdfrancis@gmail.com>
2017-11-01 18:46:27 -0600

    adds features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms'
+// import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +9,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   comments = [
-      'first comment!',
-      'nice work!',
-      'I would also like to congratulate you!'
+      {
+      	phrase: 'first comment!',
+      	author: 'Galileo Galilei'
+      },
+      {
+      	phrase: 'nice work!',
+      	author: 'Nicola Tesla'
+      },
+      {
+      	phrase: 'I would also like to congratulate you!',
+      	author: 'Albert Einstein'
+      }
   ];
+
+  edit_value = true;
+
+
+  constructor () {}
+
+  addComment(phrase, author) {
+  	let newComment = {
+  		phrase: phrase,
+  		author: author
+  	}; 
+  	this.comments.push(newComment);
+  }
+
+  deleteComment(comment) {
+  	for(let i=0; i<this.comments.length; i++) {
+  		if(this.comments[i].phrase === comment.phrase) {
+  			this.comments.splice(i, 1);
+  		}
+  	}
+  }
+
+  editComment() {
+  	this.edit_value = null;
+  }
+
+
 }
+
+
